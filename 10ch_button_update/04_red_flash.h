b8466dbe925d6d5172dfc516f918072320376082
krida0electronics <50166657+krida0electronics@users.noreply.github.com>
2023-11-19 08:08:55 +0200

    Add files via upload

---
+++ 10ch_button_update/04_red_flash.h
@@ -0,0 +1,10 @@
+ 
+ digitalWrite(RED_L1 , HIGH); digitalWrite  (RED_L4, HIGH);
+ digitalWrite(DONT_WALK_L7, HIGH); digitalWrite(DONT_WALK_L9, HIGH); 
+
+ delay(500);
+
+ digitalWrite(RED_L1, LOW); digitalWrite(RED_L4, LOW);
+ digitalWrite(DONT_WALK_L7, LOW);  digitalWrite(DONT_WALK_L9, LOW); 
+
+ delay(500);
---
+++ 10ch_button_update/05_all_flash.h
@@ -0,0 +1,15 @@
+
+
+ digitalWrite(RED_L1 , HIGH); digitalWrite  (RED_L4, HIGH);
+ digitalWrite(YELLOW_L2, HIGH); digitalWrite(YELLOW_L5, HIGH); 
+ digitalWrite(GREEN_L3, HIGH); digitalWrite(GREEN_L6, HIGH);
+ digitalWrite(DONT_WALK_L7, HIGH); digitalWrite(DONT_WALK_L9, HIGH); 
+ digitalWrite(WALK_L8, HIGH); digitalWrite(WALK_L10, HIGH); 
+ delay(500);
+
+ digitalWrite(RED_L1 , LOW); digitalWrite  (RED_L4, LOW);
+ digitalWrite(YELLOW_L2, LOW); digitalWrite(YELLOW_L5, LOW); 
+ digitalWrite(GREEN_L3, LOW); digitalWrite(GREEN_L6, LOW);
+ digitalWrite(DONT_WALK_L7, LOW); digitalWrite(DONT_WALK_L9, LOW); 
+ digitalWrite(WALK_L8, LOW); digitalWrite(WALK_L10, LOW); 
+ delay(500);
